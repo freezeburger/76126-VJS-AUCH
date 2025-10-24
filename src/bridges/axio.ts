80974bbd79f24ee3560a00ee41080aa23d1e46fb
freezeburger <renaud.dubuis@decryptage.net>
2025-10-24 11:54:20 +0200

    axios configuration

---
+++ src/bridges/axio.ts
@@ -0,0 +1,27 @@
+
+import axios from 'axios';
+
+export const http = axios.create({
+  baseURL: 'https://localhost:5050',
+  timeout: 1000,
+  headers: { 
+    'X-Custom-Header': 'foobar' 
+  }
+});
+
+http.interceptors.request.use( (config) => {
+    console.log('Request sent at: ', new Date().toISOString(), config);
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  },
+  { synchronous: true}
+);
+
+http.interceptors.response.use((response) => {
+    console.log('Response received at: ', new Date().toISOString(), response);
+    return response;
+  }, function onRejected(error) {
+    return Promise.reject(error);
+  });
+
