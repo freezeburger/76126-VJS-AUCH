18ccc3664719283c309dbc687daf28904ba0f54c
freezeburger <renaud.dubuis@decryptage.net>
2025-10-24 09:45:04 +0200

    Component Declaration

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import './style.css'
 import App from './App.vue'
 import { router } from './routes'
 
+import * as UI from '@/components';
+
 const app = createApp(App)
+
+Object.values(UI).forEach((component) => {
+  app.component((component as any).__name.split('.')[0], component);
+});
+
+
 app.use(router)
 
 app.mount('#app')
